e7c98bc89c920d635dc85b386d319b6297293896
Benjamin <benjamind10@pm.me>
2023-05-21 23:44:57 -0400

    added build paths

--- server/server.js
+++ server/server.js
@@ -44,6 +44,16 @@ app.use((req, res, next) => {
   next();
 });
 
+// Accessing the path module
+const path = require('path');
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, './client/build')));
+// Step 2:
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
 // Routes
 app.use('/api/posts', blogPostController);
 app.use('/api/user', userController);
